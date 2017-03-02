fc86ef7c250dedcc9c5b9dedbea55a6045f0b094
Ana Barreto <anabarreto@Anas-MacBook-Pro.local>
2017-03-02 21:43:53 +0000

    Complete js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,23 +1,40 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-/* 
-if (cardTwo === cardFour) {
-	alert ('You found a match');
-} else {
-	alert ('Sorry try again');
-}
- */
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = []
 
 function createCards() {
 	var gameBoard = document.getElementById("game-board");
 
-	for (var i = 0; i < 4; i++) {
+	for (var i = 0; i < cards.length; i++) {
 	  var card = document.createElement("div");
 	  card.className = "card";
+	  card.setAttribute("data-card", cards[i]);
+	  card.addEventListener("click", isTwoCards);
   	gameBoard.appendChild(card);
 	}
 }
 
 createCards();
+
+function isMatch() {
+  var cards = document.querySelectorAll(".card");
+  for (var i = 0; i < cards.length; i++) {
+  	cards[i].innerHTML = "";
+  }
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("YOU FOUND A MATCH");
+	} else {
+		alert("TRY AGAIN");
+	}
+}
+
+function isTwoCards() {
+	this.innerHTML = "<img width='100%' src='" + this.getAttribute("data-card") + ".png'>";
+
+	cardsInPlay.push(this.getAttribute("data-card"));
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
