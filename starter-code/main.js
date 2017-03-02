ec4058bb401b17b304bf746247131c3f048ff52a
Ana Barreto <anabarreto@Anas-MacBook-Pro.local>
2017-03-02 14:54:25 +0000

    Added Javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,23 @@
 var cardOne = "queen";
 var cardTwo = "queen";
-var cardThree = "queen";
+var cardThree = "king";
 var cardFour = "king";
-var cardFive = "king";
-var cardSix = "king";
+/* 
+if (cardTwo === cardFour) {
+	alert ('You found a match');
+} else {
+	alert ('Sorry try again');
+}
+ */
+
+function createCards() {
+	var gameBoard = document.getElementById("game-board");
+
+	for (var i = 0; i < 4; i++) {
+	  var card = document.createElement("div");
+	  card.className = "card";
+  	gameBoard.appendChild(card);
+	}
+}
+
+createCards();
